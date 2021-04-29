54b639e7fdab4e50f13f71da28184bc536a00ea1
sammyGUBA <baltazarsamuel420@gmail.com>
2021-04-29 13:35:44 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -38,33 +38,18 @@ export default function Home({ navigation }) {
                         justifyContent: 'center'
                     }
                 }>
-                    
+
                     <Image source={require('../assets/iscorLogo.png')} style={{ height: 50, width: 150, marginTop: 30, marginBottom: 15 }}/>    
                     
                     <Section style={{marginTop: 30, width: "95%"}}>
                         <SectionContent>
-                            <Text size="xl" fontWeight="bold"> Inicia sesión en tu cuenta de ISCOR</Text>
-                            <View style={{marginTop: 45}}>
-                                <Text>Correo Electrónico</Text>
-                                <TextInput
-                                    placeholder="example@example.com"
-                                    keyboardType="email-address"
-                                    style={{marginTop: 10, borderColo: '#d8d8d8', borderRadius: 8, backgroundColor: '#ffffff'}}
-                                />
-                            </View>
-                            <View style={{marginTop: 25}}>
-                                <Text>Contraseña</Text>
-                                <TextInput
-                                    placeholder='************'
-                                    secureTextEntry={true}
-                                    style={{marginTop: 10}}
-                                />
-                            </View>
+                            <Text size="xl" fontWeight="bold"> HOME </Text>
 
                             <View style={{marginTop: 45}}>
                                 <Button text="Iniciar Sesión" color="#997a3c" status="info"/>
-                                <Button text="Registrate" color="#997a3c" outline style={{marginTop: 10}} onPress={() => navigation.navigate('Registro')}/>
-                                <Text style={{marginTop: 40}}> Recuperar Contraseña</Text>
+                                <Button text="Invitar" color="#997a3c" outline style={{marginTop: 10}} onPress={() => navigation.navigate('Invitar')}/>
+                                <Button text="SACS" color="#997a3c" outline style={{marginTop: 10}} onPress={() => navigation.navigate('Registro')}/>
+                                <Button text="Security OneClick" color="#997a3c" outline style={{marginTop: 10}} onPress={() => navigation.navigate('SecurityOneCLick')}/>
                             </View>
                         </SectionContent>
                     </Section>
