3293ab89480e60d04977aaec8c393e697b0c4073
efrensalado <efrensalado@gmail.com>
2021-04-29 16:19:07 -0500

    Ajustes de estilos en home

--- src/Home.js
+++ src/Home.js
@@ -41,13 +41,15 @@ export default function Home({ navigation }) {
 
                     <Image source={require('../assets/iscorLogo.png')} style={{ height: 50, width: 150, marginTop: 30, marginBottom: 15 }}/>    
                     <Section style={{marginTop: 30, width: "95%"}}>
-                        <SectionContent>
+                        <SectionContent style={{alignItems: 'center', alignContent: 'center'}} >
                             <Text size="xl" fontWeight="bold"> HOME </Text>
-                            <View style={{marginTop: 45}}>
+                        </SectionContent>
+                        <SectionContent>
+                            <View style={{marginTop: 25}}>
 
                                 <Button text="Invitar" color="#997a3c" outline style={{marginTop: 10}} onPress={() => navigation.navigate('Invitar')}/>
-                                <Button text="SACS" color="#997a3c" outline style={{marginTop: 10}} onPress={() => navigation.navigate('')}/>
-                                <Button text="Security OneClick" color="#997a3c" outline style={{marginTop: 10}} onPress={() => navigation.navigate('SecurityOneClick')}/>
+                                <Button text="SACS" color="#997a3c" outline style={{marginTop: 40}} onPress={() => navigation.navigate('')}/>
+                                <Button text="Security OneClick" color="#997a3c" outline style={{marginTop: 40, marginBottom: 40}} onPress={() => navigation.navigate('SecurityOneClick')}/>
 
                             </View>
                         </SectionContent>
